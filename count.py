141acde0f541c2bad4251bdb8ad016230771da6b
mergitto <merugitosu@gmail.ccom>
2018-07-30 22:43:47 +0900

    出現頻度上位10の単語を表示するようにした

--- count.py
+++ count.py
@@ -6,22 +6,24 @@
 
 import pandas as pd
 import sys
+from collections import Counter
 
 FILENAME = sys.argv[1]
 
 df = pd.read_csv(FILENAME, header=None)
 
 documents = list(df[0])
-documents = [text.split(' ') for text in documents]
-words = []
+documents = [text.strip().split(' ') for text in documents]
 
-for text in documents:
-    for word in text:
-        words.append(word)
+words = []
+words = [word for text in documents for word in text if word != ""]
 
 print(FILENAME + "のテキスト情報を解析します")
 print("単語数:" + str(len(words)))
 print("語彙数:" + str(len(set(words))))
+
+counter_words = Counter(words)
+print(counter_words.most_common(10))
 
 
 
